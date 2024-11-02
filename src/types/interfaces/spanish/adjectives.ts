582bddd6fb963b4e6bf4f777aa12600b19c05989
lila-app <lila.language.app@gmail.com>
2024-11-02 22:22:43 +0100

    adding types interfoce for spanish adjectives

---
+++ src/types/interfaces/spanish/adjectives.ts
@@ -0,0 +1,21 @@
+export interface SpanishAdjectives {
+  id: string;
+  created_at: Date;
+  adjective_singular_masculine: string;
+  ipa_singular_masculine: string;
+  audio_links_singular_masculine: string[];
+  adjective_singular_feminine: string;
+  ipa_singular_feminine: string;
+  audio_links_singular_feminine: string[];
+  adjective_plural_masculine: string;
+  ipa_plural_masculine: string;
+  audio_links_plural_masculine: string[];
+  adjective_plural_feminine: string;
+  ipa_plural_feminine: string;
+  audio_links_plural_feminine: string[];
+  difficulty: "easy" | "middle" | "hard";
+  english_translations: string[];
+  french_translations: string[];
+  italian_translations: string[];
+  german_translations: string[];
+}
