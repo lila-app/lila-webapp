72589687ab0428eab87b5d888ece0b039eae925c
lila-app <lila.language.app@gmail.com>
2024-11-02 22:37:10 +0100

    refactoring types file for spanish nouns

--- src/types/interfaces/spanish/nouns.ts
+++ src/types/interfaces/spanish/nouns.ts
@@ -1,11 +1,16 @@
 export interface SpanishNoun {
   id: string;
-  word: string;
-  ipa: string;
+  created_at: Date;
+  noun_singular: string;
+  ipa_singular: string;
+  audio_links_singular: string;
+  syllabification_singular: string;
+  noun_plural: string;
+  ipa_plural: string;
+  audio_links_plural: string;
+  syllabification_plural: string;
   grammaticalGender: "masculine" | "feminine";
-  plural: string;
-  syllabification: string;
-  linkToAudioFile: string;
+  difficulty: "easy" | "middle" | "hard";
   english_translations: string[];
   french_translations: string[];
   italian_translations: string[];
